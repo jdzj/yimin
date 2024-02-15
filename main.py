1a636a045f0b9c8e17b649d4c97847c43c179d27
jdzj <topy@vip.qq.com>
2024-02-15 14:26:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from github import Github
 from lxml.etree import CDATA
 from marko.ext.gfm import gfm as marko
 
-MD_HEAD = """## Gitblog
+MD_HEAD = """## 移民新闻网
 移民新闻网(ymxww.com)主要分享关于移民的政策，信息，为方便有需要的人了解。
 欢迎大家共同提供有价值的内容，发布在 issues 即可。
 [RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
